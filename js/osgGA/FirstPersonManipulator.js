33c6d764929f272968c9fc5f6604bf317ee1b1d3
Matt Fontaine <tehqin@gmail.com>
2011-06-09 19:05:50 -0400

    Moved my changes back into first person manipulator.

--- js/osgGA/FirstPersonManipulator.js
+++ js/osgGA/FirstPersonManipulator.js
@@ -23,9 +23,15 @@ osgGA.FirstPersonManipulator.prototype = {
         this.direction = [0.0, 1.0, 0.0];
         this.angleVertical = 0.0;
         this.angleHorizontal = 0.0;
+        this.maxVerticalBound = Math.PI/2;
+        this.minVerticalBound = -Math.PI/2;
+        this.isVerticallyBound = true;
         this.eye = [0, 25.0, 10.0];
         this.up = [0, 0, 1];
         this.time = 0.0;
+        this.velocity = 5.0;
+        this.acceleration = 0.0;
+        this.maxVelocity = 40.0;
         this.buttonup = true;
     },
     reset: function()
@@ -85,10 +91,43 @@ osgGA.FirstPersonManipulator.prototype = {
         this.dx = this.dy = 0;
         this.buttonup = false;
     },
+    setVelocity: function(vel)
+    {
+        this.velocity = vel;
+    },
+    getVelocity: function()
+    {
+        return this.velocity;
+    },
+    setAcceleration: function(acc)
+    {
+        this.acceleration = acc;
+    },
+    getAcceleration: function()
+    {
+        return this.acceleration;
+    },
+    setMaxVelocity: function(vel)
+    {
+        this.maxVelocity = vel;
+    },
+    getMaxVelocity: function()
+    {
+        return this.maxVelocity;
+    },
     computeRotation: function(dx, dy)
     {
         this.angleVertical += dy*0.01;
         this.angleHorizontal -= dx*0.01;
+        
+        // Bind the vertical angle so we can't look upside down
+        if (this.isVerticallyBound)
+        {
+            if (this.angleVertical > this.maxVerticalBound)
+                this.angleVertical = this.maxVerticalBound;
+            else if (this.angleVertical < this.minVerticalBound)
+                this.angleVertical = this.minVerticalBound;
+        }
 
         var first = osg.Matrix.makeRotate(this.angleVertical, 1, 0, 0);
         var second = osg.Matrix.makeRotate(this.angleHorizontal, 0, 0, 1);
@@ -119,7 +158,7 @@ osgGA.FirstPersonManipulator.prototype = {
         var d = osg.Vec3.mult(osg.Vec3.normalize(this.direction), distance);
         this.eye = osg.Vec3.add(this.eye, d);
     },
-    strafe: function(distance)
+    moveRight: function(distance)
     {
         var cx = osg.Vec3.cross(this.direction, this.up);
         var d = osg.Vec3.mult(osg.Vec3.normalize(cx), distance);
@@ -130,19 +169,19 @@ osgGA.FirstPersonManipulator.prototype = {
         if (event.keyCode === 32) {
             this.computeHomePosition();
         } else if (event.keyCode == 87){ // W
-            this.moveForward(5.0);
+            this.moveForward(this.velocity);
             return false;
         }
         else if (event.keyCode == 83){ // S
-            this.moveForward(-5.0);
+            this.moveForward(-this.velocity);
             return false;
         }
         else if (event.keyCode == 68){ // D
-            this.strafe(5.0);
+            this.moveRight(this.velocity);
             return false;
         }
         else if (event.keyCode == 65){ // A
-            this.strafe(-5.0);
+            this.moveRight(-this.velocity);
             return false;
         }
     }
